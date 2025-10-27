048a64a2d6494a1eb47747888f446d1f9d0c3ff9
Owais <50521519+muhammad-owais-javed@users.noreply.github.com>
2025-10-27 16:08:52 +0200

    Initialize Spring Boot

---
+++ backend/src/main/java/com/protocol/confession/ConfessionApplication.java
@@ -0,0 +1,13 @@
+package com.protocol.confession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfessionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/protocol/confession/ConfessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protocol.confession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
