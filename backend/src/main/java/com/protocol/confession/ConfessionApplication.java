03f2f6c533618fdb6bd4a434dd300af6637cc200
Owais <50521519+muhammad-owais-javed@users.noreply.github.com>
2025-10-30 16:34:14 +0200

    Update

--- backend/src/main/java/com/protocol/confession/ConfessionApplication.java
+++ backend/src/main/java/com/protocol/confession/ConfessionApplication.java
@@ -3,8 +3,6 @@ package com.protocol.confession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.protocol.confession.dto.PlayerChoice;
-
 @SpringBootApplication
 public class ConfessionApplication {
 
