fd9e682be0ffa70d2495a7da1798d56d9eb559c6
Owais <50521519+muhammad-owais-javed@users.noreply.github.com>
2025-12-10 16:04:43 +0200

    Add StatModifier.java

---
+++ backend/src/main/java/com/protocol/confession/dto/StatModifier.java
@@ -0,0 +1,55 @@
+package com.protocol.confession.dto;
+
+public class StatModifier {
+
+    private Integer denial;
+    private Integer guilt;
+    private Integer confusion;
+    private Integer enlightenment;
+
+    public StatModifier(){
+
+    }
+
+    public StatModifier(Integer denial, Integer guilt, Integer confusion, Integer enlightenmnet){
+
+        this.denial = denial;
+        this.guilt = guilt;
+        this.confusion = confusion;
+        this.enlightenment = enlightenmnet;
+    }
+
+    public Integer getDenial() {
+        return denial;
+    }
+
+    public Integer getGuilt() {
+        return guilt;
+    }
+
+    public Integer getConfusion() {
+        return confusion;
+    }
+
+    public Integer getEnlightenment() {
+        return enlightenment;
+    }
+
+    public void setDenial(Integer denial) {
+        this.denial = denial;
+    }
+
+    public void setGuilt(Integer guilt) {
+        this.guilt = guilt;
+    }
+
+    public void setConfusion(Integer confusion) {
+        this.confusion = confusion;
+    }
+
+    public void setEnlightenment(Integer enlightenment) {
+        this.enlightenment = enlightenment;
+    }
+    
+    
+}
